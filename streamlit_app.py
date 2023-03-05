4c358677d61aed7d7c974db16c4e1a9e1cf6229b
andikonn <126994355+andikonn@users.noreply.github.com>
2023-03-04 21:12:23 -0500

    Update streamlit_app.py
    Added more items to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Bacon, Eggs and Cheese Bagel')
+streamlit.text('Pan Cakes and Strawberries')
+streamlit.txt('Big American Breakfast')

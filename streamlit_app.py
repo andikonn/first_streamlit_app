e9d3017b84aba1cd6acbfbb81beb5ee58af13bae
andikonn <126994355+andikonn@users.noreply.github.com>
2023-03-07 11:34:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ try:
   
   
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 #import snowflake.conneector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
